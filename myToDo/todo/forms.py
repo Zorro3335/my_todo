d8cbf9769df0733fb214878683b83a239e9e7e8a
Zorro3335 <123104276+Zorro3335@users.noreply.github.com>
2024-04-22 20:02:12 +0300

    add

---
+++ myToDo/todo/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from .models import ToDo
+class ToDoForms(ModelForm):
+    class Meta:
+        model = ToDo
+        fields = ['title', 'description', 'important']
